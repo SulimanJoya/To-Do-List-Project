cf941d7060356f2ebfa7508bb917c5b8112df8e5
Mohammad Suliman joya <sjoya66@gmail.com>
2023-01-20 17:08:39 +0430

    Fix linter erorrs

--- src/index.js
+++ src/index.js
@@ -1,30 +1,31 @@
-import "./style.css";
-const todoUl = document.getElementById("todoList");
+import './style.css';
+
+const todoUl = document.getElementById('todoList');
 
 const todoLists = [
   {
     id: 1,
-    description: "Morning session",
+    description: 'Morning session',
     completed: true,
   },
   {
     id: 2,
-    description: "First Program time meeting",
+    description: 'First Program time meeting',
     completed: true,
   },
   {
     id: 3,
-    description: "Second Program time meeting",
+    description: 'Second Program time meeting',
     completed: false,
   },
   {
     id: 4,
-    description: "JavaScript packages management",
+    description: 'JavaScript packages management',
     completed: true,
   },
   {
     id: 5,
-    description: "Lunch break",
+    description: 'Lunch break',
     completed: false,
   },
 ];
@@ -32,23 +33,23 @@ const todoLists = [
 const displayTodos = () => {
   for (let i = 0; i < todoLists.length; i += 1) {
     const item = todoLists[i];
-    const itemContainer = document.createElement("li");
-    itemContainer.className = "todo";
-    const todoTitle = document.createElement("h3");
-    todoTitle.className = "todo_title";
+    const itemContainer = document.createElement('li');
+    itemContainer.className = 'todo';
+    const todoTitle = document.createElement('h3');
+    todoTitle.className = 'todo_title';
     todoTitle.innerText = item.description;
-    const checkBox = document.createElement("input");
-    checkBox.type = "checkbox";
+    const checkBox = document.createElement('input');
+    checkBox.type = 'checkbox';
     checkBox.checked = item.completed;
-    checkBox.className = "checkbox";
-    const option = document.createElement("span");
-    option.className = "Refresh-icone";
-    option.innerText = "more";
+    checkBox.className = 'checkbox';
+    const option = document.createElement('span');
+    option.className = 'Refresh-icone';
+    option.innerText = 'more';
 
-    const div = document.createElement("div");
+    const div = document.createElement('div');
     div.appendChild(checkBox);
     div.appendChild(todoTitle);
-    div.className = "todo_right";
+    div.className = 'todo_right';
 
     itemContainer.appendChild(div);
     itemContainer.appendChild(option);
@@ -57,7 +58,7 @@ const displayTodos = () => {
   }
 };
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   displayTodos();
   return false;
 });
