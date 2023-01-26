75f6b7036612d390ef7e0e78a66d7f4a96e9730d
Mohammad Suliman joya <sjoya66@gmail.com>
2023-01-26 11:00:42 +0430

    update by adding other modules

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import './style.css';
-import createTodo from '../modules/create.js';
-// import NewTodo from '../modules/todoClass.js';
+import "./style.css";
+import createTodo from "../modules/create.js";
+import { deleteCompletedTodos } from "../modules/crud2.js";
 
-const form = document.getElementById('form');
-export const todo = document.querySelector('.add_todo');
-export const todoUl = document.getElementById('todoList');
-export const todoListss = JSON.parse(localStorage.getItem('todos')) || [];
+const form = document.getElementById("form");
+export const todo = document.querySelector(".add_todo");
+export const todoUl = document.getElementById("todoList");
+export const todoListss = JSON.parse(localStorage.getItem("todos")) || [];
+const deleteButton = document.getElementById("clear_button");
+
+// functionality to clear all the todos
+deleteButton.addEventListener("click", () => {
+  deleteCompletedTodos(todoListss, todoUl);
+});
 
 class NewTodo {
   constructor(description) {
@@ -19,18 +25,18 @@ class NewTodo {
       description: this.description,
       completed: false,
       disabled: true,
-      icon: 'more_vert',
+      icon: "more_vert",
     };
     createTodo(item, todoListss, todoUl);
     todoListss.push(item);
   };
 }
 
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   const newTodo = new NewTodo(todo.value);
   newTodo.add();
-  localStorage.setItem('todos', JSON.stringify(todoListss));
+  localStorage.setItem("todos", JSON.stringify(todoListss));
   form.reset();
 });
 
@@ -43,7 +49,7 @@ export const displayTodos = () => {
 
 // localStorage.removeItem('todos');
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   displayTodos();
   return false;
 });
