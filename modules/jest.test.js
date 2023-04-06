909e50acb1f5a6adfb082daaeea3d8e3dd323140
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-04-06 14:04:14 +0530

    add test for setItem in localStorage

--- modules/jest.test.js
+++ modules/jest.test.js
@@ -21,4 +21,13 @@ describe('TodoApp', () => {
     todos = window.document.querySelectorAll('.todo');
     expect(todos).toHaveLength(1);
   });
+
+  it('should add one new item to the localstorage', () => {
+    document.body.innerHTML = `
+     <ul id="todoList"><li></li></ul>' `;
+    const todoUl = document.getElementById('todoList');
+    const todoListss = JSON.parse(window.localStorage.getItem('todos')) || [];
+    addTodo('this is the first todo', 1, todoListss, todoUl);
+    expect(todoListss).toHaveLength(1);
+  });
 });
