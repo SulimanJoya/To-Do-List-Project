777339a62735da3182aacbc6e2b0f44ecbf0336a
KUBAHO LINNE Heaven <kubaholinne@gmail.com>
2023-04-06 14:02:31 +0530

    add addTodo module test

---
+++ modules/jest.test.js
@@ -0,0 +1,24 @@
+import addTodo from './addTodo.js';
+import { deleteTodo } from './crud.js';
+import { selectTodo } from './crud2.js';
+/**
+ * @jest-environment jsdom
+ */
+
+describe('TodoApp', () => {
+  beforeEach(() => {
+    window.localStorage.clear();
+  });
+
+  let todos = window.document.querySelectorAll('.todo');
+
+  it('should add one new item to the DOM', () => {
+    document.body.innerHTML = `
+     <ul id="todoList"><li></li></ul>' `;
+    const todoUl = document.getElementById('todoList');
+    const todoListss = JSON.parse(window.localStorage.getItem('todos')) || [];
+    addTodo('this is the first todo', 1, todoListss, todoUl);
+    todos = window.document.querySelectorAll('.todo');
+    expect(todos).toHaveLength(1);
+  });
+});
